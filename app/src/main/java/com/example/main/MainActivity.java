6081274420d8f0267b2784913f05bb85a45e743e
vano <problem03@yandex.ru>
2020-02-28 13:47:40 +0500

    0.0.1

--- app/src/main/java/com/example/main/MainActivity.java
+++ app/src/main/java/com/example/main/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.main;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.textView) {
+                Intent intent= new Intent(MainActivity.this,Registration.class);
+                startActivity(intent);
+            }
     }
 }
